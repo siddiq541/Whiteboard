9ebc27b9946ec2e295026a1789a307945900a819
Muhammad Siddiq <m4siddiq@gmail.com>
2019-04-04 09:25:10 +0100

    added new feature

---
+++ Whiteboardv0001/app/src/main/java/com/example/whiteboardv0001/MyFirebaseInstanceService.java
@@ -0,0 +1,63 @@
+package com.example.whiteboardv0001;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v4.media.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import static android.support.constraint.Constraints.TAG;
+
+public class MyFirebaseInstanceService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        showNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
+    }
+
+    private void showNotification(String title, String body) {
+        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        String NOTIFICATION_CHANNEL_ID = "com.example.whiteboardv0001.test";
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+        {
+            NotificationChannel notificationChannel  = new NotificationChannel(NOTIFICATION_CHANNEL_ID,"Notification",
+            NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Whiteboard Channel");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.GREEN);
+            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.enableLights(true);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        Notification.Builder notificationBuilder = new Notification.Builder( context: this, NOTIFICATION_CHANNEL_ID);
+        notificationBuilder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentInfo("Info");
+
+    }
+
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.d(TAG, "Refreshed token: " + s);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+       // sendRegistrationToServer(token);
+    }
+}
