2718339c240da864d3057b441f9a1e288b5ec97d
Alastair <a.scott555@live.co.uk>
2019-04-06 12:46:00 +0100

    Added Database helper class
    Class uses SQLite to store messages (no edit / delete functionality yet)

---
+++ Whiteboardv0001/app/src/main/java/com/example/whiteboardv0001/DatabaseHelper.java
@@ -0,0 +1,48 @@
+package com.example.whiteboardv0001;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "messages.db";
+    public static final String TABLE_NAME = "messages_table";
+    public static final String COL1 = "MESSAGE_ID";
+    public static final String COL2 = "USER_ID";
+    public static final String COL3 = "MESSAGE_STRING";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String  createTable = "CREATE TABLE " + TABLE_NAME + "(MESSAGE_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                              "USER_ID TEXT, MESSAGE_STRING TEXT)";
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean addData(String userID, String message){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(COL2,userID);
+        contentValues.put(COL2,message);
+
+        // Check if data added successfully, a result of -1 means there is an error and
+        // the data was not added to the database.
+        long result = db.insert(TABLE_NAME, null, contentValues);
+
+        if (result == -1){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
