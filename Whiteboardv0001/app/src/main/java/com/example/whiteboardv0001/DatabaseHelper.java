3b29ef551bb3e84ff04322cbd97ddf9ee79cd648
Alastair <a.scott555@live.co.uk>
2019-04-06 13:51:18 +0100

    Added channel and room id to DB table

--- Whiteboardv0001/app/src/main/java/com/example/whiteboardv0001/DatabaseHelper.java
+++ Whiteboardv0001/app/src/main/java/com/example/whiteboardv0001/DatabaseHelper.java
@@ -10,7 +10,9 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "messages_table";
     public static final String COL1 = "MESSAGE_ID";
     public static final String COL2 = "USER_ID";
-    public static final String COL3 = "MESSAGE_STRING";
+    public static final String COL3 = "CHANNEL_ID";
+    public static final String COL4 = "ROOM_ID";
+    public static final String COL5 = "MESSAGE_STRING";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -19,7 +21,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String  createTable = "CREATE TABLE " + TABLE_NAME + "(MESSAGE_ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                              "USER_ID TEXT, MESSAGE_STRING TEXT)";
+                              "USER_ID TEXT, CHANNEL_ID TEXT, ROOM_ID TEXT, MESSAGE_STRING TEXT)";
         db.execSQL(createTable);
     }
 
@@ -29,11 +31,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean addData(String userID, String message){
+    public boolean addData(String userID, String channelID, String roomID, String message){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COL2,userID);
-        contentValues.put(COL2,message);
+        contentValues.put(COL3,channelID);
+        contentValues.put(COL4,roomID);
+        contentValues.put(COL5,message);
 
         // Check if data added successfully, a result of -1 means there is an error and
         // the data was not added to the database.
