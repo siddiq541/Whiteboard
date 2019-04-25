a8ba62b8c84240c4970335da4326aad8dde15009
Alastair <a.scott555@live.co.uk>
2019-04-25 10:44:25 +0100

    Added code to pass userRole from SignIn to MainActivity
    The database still needs to be queried to get the userRole however, I've just added a placeholer userRole of "admin"

--- app/src/main/java/com/example/whiteboardv0001/SignInActivity.java
+++ app/src/main/java/com/example/whiteboardv0001/SignInActivity.java
@@ -59,8 +59,18 @@ public class SignInActivity extends AppCompatActivity {
         mAuthListner = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                if (firebaseAuth.getCurrentUser() !=null)
-                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                // <Get userRole from Firebase realtime database, place it in userRole var below>
+                String userRole = "admin"; // e.g this would be our result for an admin log in.
+
+                if (firebaseAuth.getCurrentUser() !=null ) {
+                    Intent mainIntent = new Intent(SignInActivity.this, MainActivity.class); // Create Intent
+                    mainIntent.putExtra("userRole", userRole); // place our userRole variable into our intent
+                    startActivity(mainIntent); // Start and switch to MainActivity.java, passing our user role with it.
+
+                    // To get our userRole value while inside MainActivity.java, we use:
+                    //     Bundle extras = getIntent().getExtras(); // Get the entire extras array
+                    //     String value = extras.getString("key"); // Access our userRole using it's "key" userRole
+                }
             }
         };
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
