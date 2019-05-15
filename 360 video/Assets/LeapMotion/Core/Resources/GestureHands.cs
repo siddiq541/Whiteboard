22101cd8c32adb284ab47d594c40cec52b988eb7
Alastair <a.scott555@live.co.uk>
2019-05-15 13:40:44 +0100

    Added scene/mouse control(y), removed gesturehands code,

--- 360 video/Assets/LeapMotion/Core/Resources/GestureHands.cs
+++ 360 video/Assets/LeapMotion/Core/Resources/GestureHands.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Leap;
@@ -42,3 +42,4 @@ public class GestureHands : MonoBehaviour
 
     }
 }
+*/
